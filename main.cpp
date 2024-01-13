3c1dd0dc9871efa59966ef1a2a5d4a98962b1967
Bilnux <bilgame91330@gmail.com>
2024-01-13 03:39:48 +0100

    Clearing repo
    Last clearing of the repo. End of the project.

--- main.cpp
+++ main.cpp
@@ -11,6 +11,8 @@ int main() {
     double* delta_t;
 
     delta_t = myOption.delta_trajectory(s, N);
+    cout << myOption << " le prix de l'option " << endl;
+    myOption.MonteCarloSimulation(1e6, true);
 //    for(int i=0; i < n; i++){
 //        cout << delta_t[i] << endl;
 //    }
